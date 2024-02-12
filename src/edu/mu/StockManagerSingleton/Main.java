1d580960eeb23bc079117f09e2460335aa8204a8
Aditya Ramakrishnan <112342054+aditya-ramki04@users.noreply.github.com>
2024-02-12 17:20:49 -0600

    initalizeStock method created

--- src/edu/mu/StockManagerSingleton/Main.java
+++ src/edu/mu/StockManagerSingleton/Main.java
@@ -3,6 +3,8 @@ package edu.mu.StockManagerSingleton;
 public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		StockManagerSingleton manager = new StockManagerSingleton();
+		manager.initializeStock();
+		
 	}
 }
--- src/edu/mu/StockManagerSingleton/StockManagerSingleton.java
+++ src/edu/mu/StockManagerSingleton/StockManagerSingleton.java
@@ -1,24 +1,23 @@
 package edu.mu.StockManagerSingleton;
-
-import edu.mu.MediaProduct.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
+import edu.mu.MediaProduct.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 
 public class StockManagerSingleton {
 
 	
 	private static StockManagerSingleton instance = null;
 	// Define paths as final so that they cannot be modified later.
-	private static String inventoryFilePath;
-	private List<MediaProduct> inventory;
+	private static String inventoryFilePath = "inventory.csv";
+	private ArrayList<MediaProduct> inventory = new ArrayList<MediaProduct>();
 	
 	
-	private StockManagerSingleton() 
+	public StockManagerSingleton() 
 	{
-		this.inventoryFilePath = "inventory.csv";
+		
 	}
 	    
 	public static synchronized StockManagerSingleton getInstance() 
@@ -30,10 +29,42 @@ public class StockManagerSingleton {
         return instance;
     }
 
-//	public boolean initializeStock()
-//	{
-//		return true;
-//	}
+	public boolean initializeStock()
+	{
+		try {
+			Scanner fileIn = new Scanner(new FileInputStream(inventoryFilePath)); //scans the file.
+			
+			fileIn = new Scanner(new FileInputStream(inventoryFilePath)); // reopens the file to start from the beginning again.
+			
+			int index = 0; // index variable to go through each student
+			
+			if (fileIn.hasNextLine()) {
+	            fileIn.nextLine(); // Skip the header line
+	        }
+			while(fileIn.hasNextLine()) 
+			{
+				String line = fileIn.nextLine(); //gets the next line to hold the whole line 
+				String parts[] = line.split(","); //splits the line at each " " so now the parts array has the id, Name, grade
+				String title = parts[1];
+				double price = Double.parseDouble(parts[2]);
+				int year = Integer.parseInt(parts[3]); //using parseInt to parse it from String to int and then storing it in id.
+				Genre genre = Genre.valueOf(parts[4]);
+				inventory.add(new MediaProduct(title, price, year, genre));
+			}
+			fileIn.close();//closes file
+			
+			/* testing purposes
+			 *  for(int i = 0; i < inventory.size(); i++) {
+			 * System.out.println(inventory.get(i)); }
+			 */
+			return true;
+		} catch (FileNotFoundException e) //if file not found then return no file found and return false.
+		{
+			System.out.println("No File Found");
+			e.printStackTrace();
+			return false;
+		}
+	}
 	
 	public boolean updateItemPrice(MediaProduct product, double newPrice)
 	{

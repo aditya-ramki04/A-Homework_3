a6ff11167d471db0388f6007322a8a6669d10388
Matthew Robison <mattrobi04@gmail.com>
2024-02-12 15:55:09 -0600

    completed updateItemPrice function as well as implemented getters and setters into MediaProducts

--- src/edu/mu/MediaProduct/MediaProduct.java
+++ src/edu/mu/MediaProduct/MediaProduct.java
@@ -21,6 +21,38 @@ public class MediaProduct {
         this.genre = copy.genre;
     }
 
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+
 }
 
 
--- src/edu/mu/StockManagerSingleton/StockManagerSingleton.java
+++ src/edu/mu/StockManagerSingleton/StockManagerSingleton.java
@@ -1,11 +1,19 @@
 package edu.mu.StockManagerSingleton;
 
+import edu.mu.MediaProduct.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class StockManagerSingleton {
 
 	
 	private static StockManagerSingleton instance = null;
 	// Define paths as final so that they cannot be modified later.
 	private static String inventoryFilePath;
+	private List<MediaProduct> inventory;
 	
 	
 	private StockManagerSingleton() 
@@ -26,10 +34,24 @@ public class StockManagerSingleton {
 //	{
 //		return true;
 //	}
-//	public boolean updateItemPrice(MediaProduct product, double newPrice)
-//	{
-//		return true;
-//	}
+	
+	public boolean updateItemPrice(MediaProduct product, double newPrice)
+	{
+		for (MediaProduct item : inventory) 
+		{
+            if (item.equals(product)) 
+            {
+                // Update the price
+                item.setPrice(newPrice);
+                return true; // Update successful
+            }
+            
+		}
+		System.out.println("Updating the Item Price failed. Try again!");
+		return false;
+            
+	}
+	
 //	public boolean addItem(MediaProduct product)
 //	{
 //		return true;
